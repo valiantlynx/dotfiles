19155e309f6cb111a0a737392ee03abfc8b74830
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-01-05 07:13:41 +0100

    Update setup_turborepo.sh paths

--- shell/setup_turborepo.sh
+++ shell/setup_turborepo.sh
@@ -1,9 +1,9 @@
 # make a brach on the main repo named the same as the monorepo
 # add this as a subtree to the main repo
-git subtree add --prefix=packages/dotfiles https://github.com/valiantlynx/dotfiles.git main --squash
+git subtree add --prefix=packages/scipts/dotfiles https://github.com/valiantlynx/dotfiles.git main --squash
 
 # pull the subtree
-git subtree pull --prefix=packages/dotfiles https://github.com/valiantlynx/dotfiles.git main --squash
+git subtree pull --prefix=packages/scipts/dotfiles https://github.com/valiantlynx/dotfiles.git main --squash
 
 # push the subtree
-git subtree push --prefix=packages/dotfiles https://github.com/valiantlynx/dotfiles.git main
+git subtree push --prefix=packages/scipts/dotfiles https://github.com/valiantlynx/dotfiles.git main
