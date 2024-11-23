70af8a4b9b3d60148c9b7ccac06d1f21b2dcb259
Gormery Kombo Wanjiru <gormerykw@student.uia.no>
2024-11-23 09:09:12 +0100

    timezone to OSLO

--- nixos-config/modules/core/system.nix
+++ nixos-config/modules/core/system.nix
@@ -29,7 +29,7 @@
     git
   ];
 
-  time.timeZone = "Europe/Paris";
+  time.timeZone = "Europe/Oslo";
   i18n.defaultLocale = "en_US.UTF-8";
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "24.05";
