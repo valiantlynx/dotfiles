2e26fea04c197821253bfe3a8dec865c866e8879
valiantlynx <gormerykombo@gmail.com>
2024-11-29 16:23:12 +0100

    tmux is now in nix format for nixos

--- nixos-config/modules/home/default.nix
+++ nixos-config/modules/home/default.nix
@@ -1,40 +1,47 @@
-{inputs, username, host, ...}: {
+{
+  inputs,
+  username,
+  host,
+  ...
+}:
+{
   imports = [
-    ./aseprite/aseprite.nix           # pixel art editor
-    ./audacious.nix                   # music player
-    ./bat.nix                         # better cat command
-    ./browser.nix                     # firefox based browser
-    ./btop.nix                        # resouces monitor 
-    ./cava.nix                        # audio visualizer
-    ./discord/discord.nix             # discord with gruvbox
-    ./fastfetch.nix                   # fetch tool
-    ./fzf.nix                         # fuzzy finder
-    ./gaming.nix                      # packages related to gaming
-    ./git.nix                         # version control
-    ./gnome.nix                       # gnome apps
-    ./gtk.nix                         # gtk theme
-    ./hyprland                        # window manager
-    ./kitty.nix                       # terminal
-    ./swayosd.nix                     # brightness / volume wiget
-    ./swaync/swaync.nix               # notification deamon
-    ./micro.nix                       # nano replacement
-    ./nemo.nix                        # file manager
-    ./nvim.nix                        # neovim editor
+    ./aseprite/aseprite.nix # pixel art editor
+    ./audacious.nix # music player
+    ./bat.nix # better cat command
+    ./browser.nix # firefox based browser
+    ./btop.nix # resouces monitor
+    ./cava.nix # audio visualizer
+    ./discord/discord.nix # discord with gruvbox
+    ./fastfetch.nix # fetch tool
+    ./fzf.nix # fuzzy finder
+    ./gaming.nix # packages related to gaming
+    ./git.nix # version control
+    ./gnome.nix # gnome apps
+    ./gtk.nix # gtk theme
+    ./hyprland # window manager
+    ./kitty.nix # terminal
+    ./swayosd.nix # brightness / volume wiget
+    ./swaync/swaync.nix # notification deamon
+    ./micro.nix # nano replacement
+    ./nemo.nix # file manager
+    ./nvim.nix # neovim editor
     # ./logseq.nix                      # note taking app
     ./p10k/p10k.nix
-    ./packages.nix                    # other packages
-    ./retroarch.nix  
-    ./rofi.nix                        # launcher
-    ./scripts/scripts.nix             # personal scripts
-    ./spicetify.nix                   # spotify client
-    ./starship.nix                    # shell prompt
-    ./swaylock.nix                    # lock screen
-    ./viewnior.nix                    # image viewer
-    ./vscodium.nix                    # vscode forck
-    ./waybar                          # status bar
-    ./waypaper.nix                    # GUI wallpaper picker
-    ./xdg-mimes.nix                   # xdg config
-    ./yazi.nix                        # terminal file manager
-    ./zsh                             # shell
+    ./packages.nix # other packages
+    ./retroarch.nix
+    ./rofi.nix # launcher
+    ./scripts/scripts.nix # personal scripts
+    ./spicetify.nix # spotify client
+    ./starship.nix # shell prompt
+    ./swaylock.nix
+    ./tmux.nix # lock screen
+    ./viewnior.nix # image viewer
+    ./vscodium.nix # vscode forck
+    ./waybar # status bar
+    ./waypaper.nix # GUI wallpaper picker
+    ./xdg-mimes.nix # xdg config
+    ./yazi.nix # terminal file manager
+    ./zsh # shell
   ];
 }
--- nixos-config/modules/home/tmux.nix
+++ nixos-config/modules/home/tmux.nix
@@ -1,10 +1,41 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
 {
   programs.tmux = {
     enable = true;
-    clock24 = true;
-    newSession = true;
-    # Force tmux to use /tmp for sockets (WSL2 compat)
-    secureSocket = false;
+    shortcut = "m";
+    escapeTime = 0;
+    keyMode = "vi";
+    terminal = "screen-256color";
+    historyLimit = 1000000;
+
+    extraConfig = ''
+      # Terminal settings
+      set-option -sa terminal-overrides ",xterm*:Tc"
+      set -g default-terminal "screen-256color"
+      set -g terminal-overrides ',xterm-256color:RGB'
+
+      # Mouse and indexing
+      set -g mouse on
+      set -g base-index 1
+      set -g pane-base-index 1
+      setw -g pane-base-index 1
+      set -g renumber-windows on
+
+      # Styling
+      set -g pane-active-border-style 'fg=magenta,bg=default'
+      set -g pane-border-style 'fg=brightblack,bg=default'
+
+      # Status bar
+      set -g status-position top
+      set -g status-interval 1
+
+      # History
+      set -g history-limit 1000000
+
+      # Key bindings
+      bind-key -n Home send Escape "OH"
+      bind-key -n End send Escape "OF"
+    '';
   };
 }
