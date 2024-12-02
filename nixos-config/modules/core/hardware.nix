428fcd50257cd42967726882567e08b08d6c052e
valiantlynx <gormerykombo@gmail.com>
2024-12-02 20:11:57 +0100

    added bluetooth support

--- nixos-config/modules/core/hardware.nix
+++ nixos-config/modules/core/hardware.nix
@@ -7,6 +7,11 @@
 
       ];
     };
+    bluetooth = {
+      enable = true; # enables support for Bluetooth
+      powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    };
   };
+  services.blueman.enable = true;
   hardware.enableRedistributableFirmware = true;
 }
