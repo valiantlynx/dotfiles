2ca508f18cb03ee5041522e484c066e58db6713d
valiantlynx <gormerykombo@gmail.com>
2024-12-04 17:59:31 +0100

    system d boot config. should set systemd-boot.enable to true if you dont have it

--- nixos-config/modules/core/bootloader.nix
+++ nixos-config/modules/core/bootloader.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.devices = [ "nodev" ];
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
