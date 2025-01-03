9ca285ccec20b2beb2f74e2f6b08180dfec74912
valiantlynx <gormerykombo@gmail.com>
2025-01-03 14:51:13 +0100

    for if imm gonna use modules later

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -8,19 +8,19 @@
 in {
   programs.home-manager.enable = true;
 
-  home.username = "elliott";
-  home.homeDirectory = "/Users/elliott";
+  home.username = "gormery";
+  home.homeDirectory = "/Users/gormery";
   xdg.enable = true;
 
-  xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/elliott/.dotfiles/.config/nvim";
+  xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/gormery/.dotfiles/.config/nvim";
 
   home.stateVersion = "23.11";
 
   programs = {
-    tmux = import ../home/tmux.nix {inherit pkgs;};
-    zsh = import ../home/zsh.nix {inherit config pkgs lib; };
-    zoxide = (import ../home/zoxide.nix { inherit config pkgs; });
-    fzf = import ../home/fzf.nix {inherit pkgs;};
-    oh-my-posh = import ../home/oh-my-posh.nix {inherit pkgs;};
+    tmux = import ./home/tmux.nix {inherit pkgs;};
+    zsh = import ./home/zsh.nix {inherit config pkgs lib; };
+    zoxide = (import ./home/zoxide.nix { inherit config pkgs; });
+    fzf = import ./home/fzf.nix {inherit pkgs;};
+    oh-my-posh = import ./home/oh-my-posh.nix {inherit pkgs;};
   };
 }
